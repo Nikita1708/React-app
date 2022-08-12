e9b47233cead5fe1fd12e369f2a9f25889477155
Nikita <nikitaraj1710@gmail.com>
2022-08-12 11:41:20 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,73 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom';
+class LoginControl extends React.Component{
+  constructor(props){
+    super(props);
+    this.handleLoginClick = this.handleLoginClick.bind(this);
+    this.handleLogoutClick = this.handleLogoutClick.bind(this);
+    this.state={isLoggedIn : false};
+}
+handleLoginClick() {
+  this.setState({isLoggedIn: true});
+}
+handleLogoutClick(){
+  this.setState({isLoggedIn: false});
+}
+
+render()
+{
+  const isLoggedIn =this.state.isLoggedIn;
+  let button;
+  if(isLoggedIn){
+    button= <LogoutButton onClick={this.handleLogoutClick} />;
+  }else{
+    button= <LoginButton onClick={this.handleLoginClick} />;
+  }
+  return(
+    <div>
+      <Greeting isLoggedIn={isLoggedIn} />;
+      {button}
+    </div>
+  );
+}
+}
+function UserGreeting(props){
+  return <h1>Welcome back!</h1>;
+}
+function GuestGreeting(props){
+  return <h1>Please sign up.</h1>;
+}
+
+
+function Greeting(props){
+  const isLoggedIn=props.isLoggedIn;
+  if(isLoggedIn){
+    return <UserGreeting />;
+  }
+    return <GuestGreeting />;
+}
+
+function LoginButton(props) {
+  return (
+    <button onClick={props.onClick}>
+      Login
+    </button>
+  );
+}
+
+function LogoutButton(props) {
+  return (
+    <button onClick={props.onClick}>
+      Logout
+    </button>
+  );
+}
+
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render(<LoginControl />);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+
